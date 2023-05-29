7c48b86fa6c4cb9ce1eb0ce5ae30d366b529ed38
koushik-ruidas <koushikruidas@gmail.com>
2023-05-29 14:46:56 +0530

    bug fix

--- src/main/java/com/projectY/splitYourBills/config/SwaggerConfig.java
+++ src/main/java/com/projectY/splitYourBills/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.projectY.splitYourBills.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
